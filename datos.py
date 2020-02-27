35aa152d2bf6fc37ac3133001b2f707c6bc1f72f
Carlos <if708830@iteso.mx>
2020-02-27 08:23:36 -0600

    Changes

---
+++ datos.py
@@ -0,0 +1,9 @@
+
+# -- ------------------------------------------------------------------------------------ -- #
+# -- Proyecto: Repaso de python 3 y analisis de precios OHLC                              -- #
+# -- Codigo: datos.py - script con datos de uso en proyecto                               -- #
+# -- Rep: https://github.com/ITESOIF/MyST/tree/master/Notas_Python/Notas_RepasoPython     -- #
+# -- Autor: Francisco ME                                                                  -- #
+# -- ------------------------------------------------------------------------------------ -- #
+
+OA_Ak = '15f72a29c535bc9eb4d8f9c3267c7a85-e152e24cb184171e7d1b098219e4c303'
